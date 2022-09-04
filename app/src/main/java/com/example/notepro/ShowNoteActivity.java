0afa7931c6eda5cdd056dba1c2b5689e6d18c3b0
Nabcellent <nabcellent.dev@gmail.com>
2022-09-04 17:58:57 +0300

    Implements add note module
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- app/src/main/java/com/example/notepro/ShowNoteActivity.java
+++ app/src/main/java/com/example/notepro/ShowNoteActivity.java
@@ -6,6 +6,10 @@ import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.notepro.models.Note;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.DocumentReference;
+
 public class ShowNoteActivity extends AppCompatActivity {
     EditText titleInput, contentInput;
     ImageButton saveBtn;
@@ -26,9 +30,29 @@ public class ShowNoteActivity extends AppCompatActivity {
         String title = titleInput.getText().toString();
         String content = contentInput.getText().toString();
 
-        if(title == null || title.isEmpty()) {
+        if (title == null || title.isEmpty()) {
             titleInput.setError("Title is required.");
             return;
         }
+
+        Note note = new Note();
+        note.setTitle(title);
+        note.setContent(content);
+        note.setTimestamp(Timestamp.now());
+
+        saveToFirebase(note);
+    }
+
+    void saveToFirebase(Note note) {
+        DocumentReference documentReference = Utility.getCollectionReferenceForNotes().document();
+
+        documentReference.set(note).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                Utility.showToast(this, "Note added successfully!");
+                finish();
+            } else {
+                Utility.showToast(this, "Failed to add note!");
+            }
+        });
     }
 }
--- app/src/main/java/com/example/notepro/Utility.java
+++ app/src/main/java/com/example/notepro/Utility.java
@@ -3,8 +3,20 @@ package com.example.notepro;
 import android.content.Context;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class Utility {
     static void showToast(Context c, String message) {
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
+    }
+
+    static CollectionReference getCollectionReferenceForNotes() {
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        return FirebaseFirestore.getInstance().collection("notes").document(currentUser.getUid())
+                                .collection("notes");
     }
 }
