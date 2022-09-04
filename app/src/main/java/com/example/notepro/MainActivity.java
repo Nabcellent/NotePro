fadd352074abdd763261a661496daeb91dc5c91a
Nabcellent <nabcellent.dev@gmail.com>
2022-09-04 16:57:59 +0300

    Implements add note module
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- app/src/main/java/com/example/notepro/MainActivity.java
+++ app/src/main/java/com/example/notepro/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.notepro;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
+    FloatingActionButton addNoteBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addNoteBtn = findViewById(R.id.add_note_btn);
+
+        addNoteBtn.setOnClickListener(v -> startActivity(new Intent(this, ShowNoteActivity.class)));
     }
 }
---
+++ app/src/main/java/com/example/notepro/ShowNoteActivity.java
@@ -0,0 +1,14 @@
+package com.example.notepro;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ShowNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_note);
+    }
+}
